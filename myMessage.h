20c5ee3adf2013b9565eef2d31da2af97b764fd4
Chen Chen <cchenc@cn.ibm.com>
2014-09-22 23:16:52 -0400

    split the message from the process

---
+++ myMessage.h
@@ -0,0 +1,26 @@
+#ifndef MYMESSAGE_H
+#define MYMESSAGE_H
+
+enum msg_type {BYZANTINE = 1, ACK = 2};
+
+/* The structure of the Byzantine message */
+typedef struct {
+	uint32_t type; // Must be equal to 1
+	uint32_t size; // size of message in bytes
+	uint32_t round; // round number
+	uint32_t order; // the order (retreat = 0 and attack = 1)
+	uint32_t ids[]; // id's of the senders of the message
+} ByzantineMessage;
+
+/* The sturcture of the ACK */
+typedef struct {
+	uint32_t type; // Must be equal to 2
+	uint32_t size; // size of message in bytes
+	uint32_t round; // round number
+}Ack;
+
+class myMessage{
+	
+};
+
+#endif
